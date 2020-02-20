c893c180d62e143b72202b4c1c9e50b03a28a1c8
Charles Bradley <charlesb107@email.arizona.edu>
2020-02-20 07:49:49 -0700

    Charles Bradley 03_days submission

---
+++ assignments/03_days/days.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Author : charlesb107
+Date   : 2020-02-17
+Purpose: Repeat the days of the week
+Reference: Tiny Python Projects (Book)/Biosystems Analytics (BE534), Spring 2020
+"""
+
+import argparse
+import os
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='What to do on each day',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('day',
+                        metavar='str',
+#                        nargs='+',
+                        help='Days of the week')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+
+    args = get_args()
+    day = args.day
+
+
+    days = {
+        'Monday' : 'On Mondays I never go to work',
+        'Tuesday' : 'On Tuesdays I stay at home',
+        'Wednesday' : 'On Wednesdays I never feel inclined',
+        'Thursday' : "On Thursdays, it's a holiday",
+        'Friday' : 'And Fridays I detest',
+        'Saturday' : "Oh, it's much too late on a Saturday",
+        'Sunday' : 'And Sunday is the day of rest'}
+
+    if day in days:
+        print(days.get(day, days))
+    else:
+        print(f'Can\'t find "{day}"')
+
+#---------------------------------------------------
+if __name__ == '__main__':
+    main()
