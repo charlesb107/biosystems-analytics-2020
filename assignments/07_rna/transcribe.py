6204da36d84a453f326876e80ac463b5699f88fd
Charles Bradley <charlesb107@email.arizona.edu>
2020-03-26 09:19:21 -0700

    HW07 for CB

--- assignments/07_rna/transcribe.py
+++ assignments/07_rna/transcribe.py
@@ -16,33 +16,34 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Transcribing DNA into RNA',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('-f',
-                        '--file',
-                        help='Input file(s)',
+    parser.add_argument('file',
                         metavar='FILE',
                         type=argparse.FileType('r'),
-                        required=True,
-                        default=None)
+                        help='Input file(s)')
+
 
     parser.add_argument('-o',
                         '--outdir',
                         help='Output directory',
                         metavar='DIR',
                         type=dir,
-                        default='out')
+                        default='out',
+                        required=True)
 
     args = parser.parse_args()
-
 
     base = os.path.basename(args.file)
     out_dir = 'out'
     os.path.join(out_dir, base)
-
+    # Makes an output directory if one doesn't exist
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
+
+    #args.file = open(args.file).read()
+
 
     return parser.parse_args()
 
@@ -53,31 +54,25 @@ def main():
 
     args = get_args()
 
-    for char in args.file:
-        trans_seq = char.replace('T', 'U')
-        print(trans_seq)
+
+
+
 
     for fh in args.file:
+        num_seq = 0
+        for line in fh:
+            num_seq += 1
         for char in fh:
             trans_seq = char.replace('T', 'U')
+            print(trans_seq, end='')
 
         out_file = os.path.join(out_dir, os.path.basename(fh.name))
         out_fh = open(out_file, 'wt')
         out_fh.write(trans_seq)
         out_fh.close()
 
-        num_lines = 0
-        for line in fh:
-            num_lines += 1
 
-    #print(f'Done, wrote {num_lines} sequences in {num_file} to directory "{out_dir}.')
-
-# # From Howler
-#     out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
-#     out_fh.write(args.text.upper() + '\n')
-#     out_fh.close()
-
-
+    print(f'Done, wrote {num_lines} sequences in {num_file} to directory "{out_dir}.')
 
 
 # --------------------------------------------------
