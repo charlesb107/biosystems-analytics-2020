7c88ded224e1f36ff26a2d347d9c547ddeda2cc7
Charles Bradley <charlesb107@email.arizona.edu>
2020-04-16 10:59:00 -0700

    HW10.3 for CB

--- assignments/10_unclustered_proteins/find_unclustered.py
+++ assignments/10_unclustered_proteins/find_unclustered.py
@@ -10,6 +10,7 @@ import argparse
 import os
 import sys
 import re
+from Bio import SeqIO
 
 
 # --------------------------------------------------
@@ -25,44 +26,28 @@ def get_args():
                         '--cdhit',
                         help='Output file from CD-HIT (clustered proteins)',
                         metavar='cdhit',
-                        type=str,
-                        default=None,
+                        type=argparse.FileType('rt'),
                         required=True)
 
     parser.add_argument('-p',
                         '--proteins',
                         help='Proteins FASTA',
                         metavar='proteins',
-                        #type=argparse.FileType('r'),
-                        type=str,
-                        default=None,
+                        type=argparse.FileType('rt'),
                         required=True)
 
     parser.add_argument('-o',
                         '--outfile',
                         help='Output file',
                         metavar='outfile',
-                        #type=argparse.FileType('wt'),
+                        type=argparse.FileType('wt'),
                         default='unclustered.fa')
 
 
 
     args = parser.parse_args()
 
-    if args.proteins == None:
-       parser.error(f'the following arguments are required: -p / --proteins')
-
-    if args.cdhit == None:
-       parser.error(f'the following arguments are required: -c / --cdhit')
-
-    if not os.path.isfile(args.proteins):
-        parser.error(f"No such file or directory: '{args.proteins}'")
-
-    if not os.path.isfile(args.cdhit):
-        parser.error(f"No such file or directory: '{args.cdhit}'")
-
     return args
-
 
 # --------------------------------------------------
 def main():
@@ -70,26 +55,73 @@ def main():
 
     args = get_args()
 
+## IN-CLASS EXAMPLES
 
-    out_fh = open(out_file, 'wt')
+    #Sets are lists with unique values, which are faster to parse through
+    #Sets are not in a particular order
+    clustered_ids = set()
+
+    num_written = 0
+    num_total = 0
 
     for line in args.cdhit:
-        match = re.search(r'>(\d+)', line)
-        #print(match.group(1))
-        id = match.group(1)
-        protein_ids = set()
-        protein_ids.add(id)
+        #These two lines will cause the program to ignore lines starting with '>'
+        if line.startswith('>'):
+            continue
+
+        match = re.search('>(\d+)', line)
+        if match: #Only prints matches
+            prot_id = match.group(1)
+            #Sets the dict values to 1
+            #clustered_ids[prot_id] = 1
+
+
 
     for rec in SeqIO.parse(args.proteins, 'fasta'):
-        protein_id = re.sub(r'\|.*', '', id)
-        if protein_id not in protein_ids:
-            SeqIO.write(rec, out_fh, 'fasta')
-        #print(rec)
-            break
+        num_total += 1
+        # match = re.search('>(\d+)', rec.id)
+        # if match:
+        #     prot_id = match.group(1)
+
+        #This replaces
+        prot_id = re.sub('\|.*', '', rec.id)
+
+        if prot_id not in clustered_ids:
+            num_written += 1
+            SeqIO.write(rec, args.outfile, 'fasta')
 
 
-    plural = "" if unclust_total == 1 else 's'
-    print(f'Wote {unclust_num} of {unclust_total} unclustered protein{plural} to "{args.outfile}"')
+    print(f'Wrote {num_written:,} of {num_total:,} unclustered proteins to "{args.outfile.name}"')
+
+
+
+
+
+
+
+
+
+    # out_fh = open(args.outfile, 'wt')
+
+    #for line in args.cdhit:
+        #print(line)
+        # match = re.search(r'>(\d+)', line)
+        # print(match)
+        # id = match.group(1)
+        # protein_ids = set()
+        # protein_ids.add(id)
+        # print(protein_ids)
+
+    # for rec in SeqIO.parse(args.proteins, 'fasta'):
+    #     protein_id = re.sub(r'\|.*', '', id)
+    #     if protein_id not in protein_ids:
+    #         SeqIO.write(rec, out_fh, 'fasta')
+    #     #print(rec)
+    #         break
+    #
+    #
+    # plural = "" if unclust_total == 1 else 's'
+    # print(f'Wote {unclust_num} of {unclust_total} unclustered protein{plural} to "{args.outfile}"')
 
 
 # --------------------------------------------------
